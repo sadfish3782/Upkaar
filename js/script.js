caacfce4c86bf483362e248af9ad6b482432db12
sadfish3782 <110742760+sadfish3782@users.noreply.github.com>
2024-10-05 01:13:45 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+(function($) {
+  "use strict";
+
+  $(document).ready(function() {
+    
+    // Masonry
+    initIsotope();
+
+    // Lightbox
+    lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'fitImagesInViewport': true
+    });
+    
+    // Swiper
+    var testimonialSwiper = new Swiper(".testimonial-swiper", {
+      spaceBetween: 20,
+      pagination: {
+        el: ".testimonial-swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        800: {
+          slidesPerView: 3,
+        },
+        1400: {
+          slidesPerView: 3,
+        }
+      },
+    });
+
+  }); // End of document ready
+
+  // Init Isotope
+  var initIsotope = function() {
+    $('.grid').each(function() {
+      var $buttonGroup = $('.button-group');
+      var $checked = $buttonGroup.find('.is-checked');
+      var filterValue = $checked.attr('data-filter');
+
+      var $grid = $('.grid').isotope({
+        itemSelector: '.portfolio-item',
+        filter: filterValue
+      });
+
+      // Bind filter button click
+      $('.button-group').on('click', 'a', function(e) {
+        e.preventDefault();
+        filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+      });
+
+      // Change is-checked class on buttons
+      $buttonGroup.on('click', 'a', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+      });
+    });
+  }
+})(jQuery);
